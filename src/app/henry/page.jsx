41aecdaa44242eb9650e0107a6583452fb6a10f8
Krestine H <krha0001@stud.kea.dk>
2024-04-11 11:52:23 +0200

    picture update

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getHenry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   return await res.json();
@@ -15,6 +17,13 @@ export default async function HenryPage() {
   const { name } = await getHenry();
   return (
     <main>
+      <Image
+        src="https://images.unsplash.com/photo-1517849845537-4d257902454a"
+        alt="A cute dog"
+        width={3024}
+        height={4032}
+        priority={true} // disables lazy load
+      />
       <h1>{name}</h1>
     </main>
   );
