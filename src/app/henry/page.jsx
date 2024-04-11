c5a7d105d5ba8f4a30d9a2d51a227b8d89eb94a8
Krestine H <krha0001@stud.kea.dk>
2024-04-11 10:07:13 +0200

    henry update

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,4 +1,11 @@
-export default function HenryPage() {
+async function getHenry() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  return await res.json();
+}
+
+export default async function HenryPage() {
+  const data = await getHenry();
+  console.log(data);
   return (
     <main>
       <h1>Hej</h1>
