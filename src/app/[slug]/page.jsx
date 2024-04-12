195d966f50245ad8306cf9b1ba506503cc377f69
Krestine H <krha0001@stud.kea.dk>
2024-04-12 11:23:08 +0200

    update

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,5 +1,31 @@
 import { notFound } from "next/navigation";
 
+// [slug]/page.js
+export async function generateStaticParams() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs");
+  const pages = await res.json();
+
+  const paths = pages.map((page) => {
+    return { slug: page.slug };
+  });
+
+  return paths;
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const url = `https://nice-dogs.vercel.app/api/dogs?slug=${slug}`;
+  const res = await fetch(url);
+  if (res.status !== 200) return notFound();
+
+  const data = await res.json();
+
+  return {
+    title: data.name,
+    description: `Here is ${data.name}`,
+  };
+}
+
 export default async function DogPage({ params }) {
   const { slug } = params;
 
@@ -10,5 +36,17 @@ export default async function DogPage({ params }) {
 
   const data = await res.json();
 
-  return <h1>{slug}</h1>;
+  return (
+    <main>
+      <h1>{data.name}</h1>
+      <p>
+        {data.name} is {data.age} {data.age === "1" ? "year" : "years"} old.
+      </p>
+      <p>
+        {data.favouriteColor === undefined
+          ? ""
+          : `${data.name}s favorite color is ${data.favouriteColor}.`}
+      </p>
+    </main>
+  );
 }
