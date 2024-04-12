4ceb8f9d7189fa43e3797a98effa7c5d515c9285
Krestine H <krha0001@stud.kea.dk>
2024-04-12 09:23:58 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,15 @@ export const metadata = {
   description: "allethe dogs",
 };
 
-export default function Home() {
+export default async function Home() {
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const res = await fetch(url);
+  const data = await res.json();
+
   return (
     <main>
       <h1>Hello world</h1>
+      <img src="data" alt="random dog picture" />
     </main>
   );
 }
